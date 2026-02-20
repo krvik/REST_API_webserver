88e4d010ba81bbcc8776ac7d5ec19b554ca0fb13
EC2 Default User <ec2-user@ip-172-31-32-19.ap-south-1.compute.internal>
2026-02-20 13:35:08 +0000

    added docker-compose.yml,updated README.md and app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from extensions import db
 from routes import students_v1, students_v2, students_legacy
+import os
+
 
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
+#    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
