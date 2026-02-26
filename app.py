6ac7ea01ba6250200385fd86b674dbdda5095652
Vikash Kumar <vikashkumardev1996@gmail.com>
2026-02-26 16:02:24 +0530

    change: allow tests to override the uri

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import os
 def create_app():
     app = Flask(__name__)
 #    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+# allow tests to override the URI
+    if not app.config.get("SQLALCHEMY_DATABASE_URI"):
+        app.config["SQLALCHEMY_DATABASE_URI"]= os.getenv("DATABASE_URL")
+        
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
