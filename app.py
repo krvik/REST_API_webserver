bba15c71a47952dab159fc1c89dc488f418488f7
EC2 Default User <ec2-user@ip-172-31-32-19.ap-south-1.compute.internal>
2026-02-26 10:49:17 +0000

    corrected flake-8 issue-------5

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def create_app():
 #    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
 # allow tests to override the URI
     if not app.config.get("SQLALCHEMY_DATABASE_URI"):
-        app.config["SQLALCHEMY_DATABASE_URI"]= os.getenv("DATABASE_URL")
-        
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
