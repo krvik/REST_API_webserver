41fed7e02156961ac4a377d496beb538760c13c2
EC2 Default User <ec2-user@ip-172-31-32-19.ap-south-1.compute.internal>
2026-02-26 11:26:59 +0000

    change: db fixed ---3

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def create_app():
         app.config.setdefault("SQLALCHEMY_DATABASE_URI", env_db)
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
     db.init_app(app)
 
     # Register routes
