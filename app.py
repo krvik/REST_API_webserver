d43510b0620100eed2d7e7e2b8c400b7c109cfdb
EC2 Default User <ec2-user@ip-172-31-32-19.ap-south-1.compute.internal>
2026-02-26 10:14:32 +0000

    corrected flake-8 issue-------4

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
 
     # Versioned
     app.register_blueprint(students_v1, url_prefix="/api/v1/students")
